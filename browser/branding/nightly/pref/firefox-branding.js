42db97f9b080f99c6edcd4d57b038eb935c498b6
sjsiskjs <Zhxnzowk9xien38dj@protonmail.com>
2026-08-16 15:18:26 +0200

    Update firefox-branding.js

--- browser/branding/nightly/pref/firefox-branding.js
+++ browser/branding/nightly/pref/firefox-branding.js
@@ -1,36 +1,32 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at http://mozilla.org. */
 
-// This file contains branding-specific prefs.
+// This file contains branding-specific prefs for Have Browser.
 
-pref("startup.homepage_override_url", "https://www.mozilla.org/projects/firefox/%VERSION%/whatsnew/?oldversion=%OLD_VERSION%");
-pref("startup.homepage_welcome_url", "https://www.mozilla.org/projects/firefox/%VERSION%/firstrun/");
+// IMPOSTAZIONE DELLA SCHEDA INIZIALE (Apre la pagina di Nuova Scheda standard)
+pref("browser.startup.homepage", "about:newtab");
+
+// DISATTIVA I COLLEGAMENTI ESTERNI E LE PAGINE DI BENVENUTO DI MOZILLA
+pref("startup.homepage_override_url", "about:blank");
+pref("startup.homepage_welcome_url", "about:blank");
 pref("startup.homepage_welcome_url.additional", "");
-// The time interval between checks for a new version (in seconds)
-pref("app.update.interval", 7200); // 2 hours
-// Give the user x seconds to react before showing the big UI. default=12 hours
-pref("app.update.promptWaitTime", 43200);
-// URL user can browse to manually if for some reason all update installation
-// attempts fail.
-pref("app.update.url.manual", "https://www.mozilla.org/%LOCALE%/firefox/nightly/?reason=manual-update");
-// A default value for the "More information about this update" link
-// supplied in the "An update is available" page of the update wizard.
-pref("app.update.url.details", "https://www.mozilla.org/%LOCALE%/firefox/nightly/notes/");
+pref("browser.startup.homepage_override.mstone", "ignore");
 
-pref("app.releaseNotesURL", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/releasenotes/?utm_source=firefox-browser&utm_medium=firefox-desktop&utm_campaign=whatsnew");
-pref("app.releaseNotesURL.aboutDialog", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/releasenotes/?utm_source=firefox-browser&utm_medium=firefox-desktop&utm_campaign=about-dialog");
-pref("app.releaseNotesURL.prompt", "https://www.mozilla.org/%LOCALE%/firefox/%VERSION%/releasenotes/?utm_source=firefox-browser&utm_medium=firefox-desktop&utm_campaign=updateprompt");
+// DISATTIVAZIONE COMPLETA DEGLI AGGIORNAMENTI (Evita che Firefox si sovrascriva ad Have)
+pref("app.update.enabled", false);
+pref("app.update.auto", false);
+pref("app.update.interval", 999999999); 
+pref("app.update.url.manual", "about:blank");
+pref("app.update.url.details", "about:blank");
 
-// The number of days a binary is permitted to be old
-// without checking for an update.  This assumes that
-// app.update.checkInstallTime is true.
-pref("app.update.checkInstallTime.days", 2);
+// DISATTIVA I LINK DELLE NOTE DI RILASCIO NEI MENU
+pref("app.releaseNotesURL", "about:blank");
+pref("app.releaseNotesURL.aboutDialog", "about:blank");
+pref("app.releaseNotesURL.prompt", "about:blank");
 
-// Give the user x seconds to reboot before showing a badge on the hamburger
-// button. default=immediately
+pref("app.update.checkInstallTime.days", 999);
 pref("app.update.badgeWaitTime", 0);
 
-// Number of usages of the web console.
-// If this is less than 5, then pasting code into the web console is disabled
+// Mantieni la console di sviluppo sbloccata
 pref("devtools.selfxss.count", 5);
